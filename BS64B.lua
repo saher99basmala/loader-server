c69afefb0306237c8ae9985f3c5d130fd82d9a8a
saher99basmala <saher11alhmed@gmail.com>
2026-05-24 02:11:12 +0300

    Update BS64B.lua

--- BS64B.lua
+++ BS64B.lua
@@ -1,56 +1,24 @@
 const express = require("express");
-const fetch = require("node-fetch");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 const SECRET = "MY_SECRET_123";
-const KEY = "XOR_SECRET_789";
 
-function xor(buffer, key) {
-  const k = Buffer.from(key);
+app.get("/script", (req, res) => {
 
-  for (let i = 0; i < buffer.length; i++) {
-    buffer[i] ^= k[i % k.length];
-  }
+if (
+req.query.key !== "12345" ||
+req.headers["x-secret"] !== SECRET
+) {
 
-  return buffer;
+return res.send("basmala");
+
 }
 
-app.get("/script", async (req, res) => {
-
-  if (req.query.key !== "12345") {
-    return res.send('gg.alert("اسكربت الجامع\\nقريباً!")');
-  }
-
-  if (req.headers["x-secret"] !== SECRET) {
-    return res.send('gg.alert("اسكربت الجامع\\nقريباً!")');
-  }
-
-  try {
-
-    const r = await fetch(
-      "ضع_رابط_الملف_الحقيقي_هنا"
-    );
-
-    let buffer = await r.buffer();
-
-    if (!buffer || buffer.length < 10) {
-      return res.send("ERROR");
-    }
-
-    const encrypted = xor(buffer, KEY);
-
-    res.send(encrypted.toString("base64"));
-
-  } catch {
-
-    res.send("ERROR");
-
-  }
+res.type("text/plain").send(
+'gg.alert("اسكربت الجامع\\nقريباً!")'
+);
 
 });
 
-app.listen(PORT, () => {
-  console.log("Server running...");
-});
+app.listen(3000);
